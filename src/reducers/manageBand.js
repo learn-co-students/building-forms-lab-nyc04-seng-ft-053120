c02762700a5c7fa6f8d0b0b4462cc2851cd3efda
AlveeM <alvee.akand@outlook.com>
2020-08-30 23:15:10 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,12 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
+  if (action.type === 'ADD_BAND') {
+    return {
+      ...state,
+      bands: [...state.bands, action.name]
+    };
+  }
+
   return state
 };
